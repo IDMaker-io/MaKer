281d1c2ec827611b0eb7f3ba027eabe09006f18c
yongjunhong <kevin0928@naver.com>
2024-05-21 22:41:58 +0900

    git init

---
+++ src/test/java/IDMaker/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package IDMaker.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
