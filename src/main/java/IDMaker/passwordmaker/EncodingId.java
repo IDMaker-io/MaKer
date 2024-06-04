305585932bdc2a1fba62689d0735a1b7d664288d
yongjunhong <kevin0928@naver.com>
2024-06-04 13:57:22 +0900

    feat : passwordMaker 기능 구현

---
+++ src/main/java/IDMaker/passwordmaker/EncodingId.java
@@ -0,0 +1,40 @@
+package IDMaker.passwordmaker;
+
+/**
+ * The enum Encoding id.
+ * @author yongjunhong
+ * @since 2024. 6. 3.
+ */
+public enum EncodingId {
+	/**
+	 *Bcrypt encoding id.
+	 */
+	BCRYPT("bcrypt"),
+	/**
+	 *Pbkdf encoding id.
+	 */
+	PBKDF("pbkdf2@SpringSecurity_v5_8"),
+	/**
+	 *Scrypt encoding id.
+	 */
+	SCRYPT("scrypt@SpringSecurity_v5_8"),
+	/**
+	 *Argon encoding id.
+	 */
+	ARGON("argon2@SpringSecurity_v5_8");
+
+	private String encodingId;
+
+	EncodingId(String encodingId) {
+		this.encodingId = encodingId;
+	}
+
+	/**
+	 * Gets encoding id.
+	 *
+	 * @return the encoding id
+	 */
+	public String getEncodingId() {
+		return encodingId;
+	}
+}
---
+++ src/main/java/IDMaker/passwordmaker/PasswordMakerListener.java
@@ -0,0 +1,84 @@
+package IDMaker.passwordmaker;
+
+import static IDMaker.idmaker.ExceptionCode.*;
+
+import java.lang.reflect.Field;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import IDMaker.idmaker.IDMakerAccessException;
+import IDMaker.idmaker.IDMakerInvalidArgumentException;
+import jakarta.persistence.PrePersist;
+
+/**
+ * Listener class for password encoding.
+ * This class listens for the PrePersist event and encodes any fields annotated with PasswordMaker.
+ *
+ * @author yongjunhong
+ * @since 2024. 6. 3.
+ */
+@Component
+public class PasswordMakerListener {
+
+	/**
+	 * Method that is called before an entity is persisted.
+	 * This method encodes any fields in the entity that are annotated with PasswordMaker.
+	 *
+	 * @param entity the entity that is about to be persisted
+	 */
+	@PrePersist
+	@Transactional
+	public void prePersist(Object entity) {
+		Field[] fields = entity.getClass().getDeclaredFields();
+
+		for (Field field : fields) {
+			if (isPasswordEncoderAnnotated(field)) {
+				EncodingField(entity, field);
+			}
+		}
+	}
+
+	/**
+	 * Checks if a field is annotated with PasswordMaker and is of type String.
+	 *
+	 * @param field the field to check
+	 * @return true if the field is annotated with PasswordMaker and is of type String, false otherwise
+	 * @throws IDMakerInvalidArgumentException if the field is annotated with PasswordMaker but is not of type String
+	 */
+	private boolean isPasswordEncoderAnnotated(Field field) {
+		if (!field.isAnnotationPresent(PasswordMaker.class)) {
+			return false;
+		}
+
+		if (field.getType() != String.class) {
+			throw new IDMakerInvalidArgumentException(IDMAKER_ANNOTATION_ON_NON_STRING.getMessage());
+		}
+
+		return true;
+	}
+
+	/**
+	 * Encodes a field in an entity.
+	 * The field is encoded using the encoding type specified in the PasswordMaker annotation on the field.
+	 *
+	 * @param entity the entity that contains the field
+	 * @param field the field to encode
+	 * @throws RuntimeException if there is an error accessing the field
+	 */
+	private void EncodingField(Object entity, Field field) throws IDMakerAccessException {
+		field.setAccessible(true);
+
+		try {
+			String originalPassword = (String)field.get(entity);
+			if (originalPassword != null && !originalPassword.isEmpty()) {
+				PasswordMaker passwordMaker = field.getAnnotation(PasswordMaker.class);
+				String encodedPassword = PasswordMakerUtil.encodingPassword(originalPassword, passwordMaker.encodingType());
+				field.set(entity, encodedPassword);
+			}
+		} catch (IllegalAccessException e) {
+			// todo : exception 정의하기
+			throw new RuntimeException();
+		}
+	}
+}
