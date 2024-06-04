51f1476c05184225990899766e811ad699772bb2
YongGoose <YongGoose@users.noreply.github.com>
2024-06-04 06:23:44 +0000

    deploy: 5cfe7a1394cfe734b4cf70255a61bea1e65e05e1

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"IDMaker.idmaker"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"IDMaker.idmaker"},{"l":"IDMaker.passwordmaker"}];updateSearchResults();
