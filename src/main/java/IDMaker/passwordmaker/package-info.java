7833c5fb5b711776edfa757f4aac6bf179553e84
yongjunhong <kevin0928@naver.com>
2024-06-04 13:57:34 +0900

    docs : package-info 작성

---
+++ src/main/java/IDMaker/passwordmaker/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Provides the classes and annotations for password encoding in the ID Maker application.
+ * This package includes classes for creating password encoders, handling password encoding listeners, and managing password encoding annotations.
+ *
+ * @author yongjunhong
+ * @since 2024. 6. 4.
+ */
+package IDMaker.passwordmaker;
