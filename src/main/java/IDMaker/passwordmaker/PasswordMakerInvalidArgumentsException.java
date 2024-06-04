4974eb588f80bf2948d5005489d29cb7a22f2744
yongjunhong <kevin0928@naver.com>
2024-06-04 15:20:35 +0900

    docs : javadoc 추가

--- src/main/java/IDMaker/passwordmaker/PasswordMakerInvalidArgumentsException.java
+++ src/main/java/IDMaker/passwordmaker/PasswordMakerInvalidArgumentsException.java
@@ -1,15 +1,26 @@
 package IDMaker.passwordmaker;
 
 /**
- *
+ * The type Password maker invalid arguments exception.
  * @author yongjunhong
  * @since 2024. 6. 4.
  */
 public non-sealed class PasswordMakerInvalidArgumentsException extends PasswordException {
+	/**
+	 * Instantiates a new Password maker invalid arguments exception.
+	 *
+	 * @param message the message
+	 * @param cause the cause
+	 */
 	public PasswordMakerInvalidArgumentsException(String message, Throwable cause) {
 		super(message, cause);
 	}
 
+	/**
+	 * Instantiates a new Password maker invalid arguments exception.
+	 *
+	 * @param message the message
+	 */
 	public PasswordMakerInvalidArgumentsException(String message) {
 		super(message);
 	}
