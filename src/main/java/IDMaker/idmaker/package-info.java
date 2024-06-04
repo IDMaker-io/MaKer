5ec054b863814c3504abfb592ee98c616eefb1e2
yongjunhong <kevin0928@naver.com>
2024-06-04 13:49:59 +0900

    docs : idmaker package-info 작성

---
+++ src/main/java/IDMaker/idmaker/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Provides the classes necessary to create ID Maker.
+ * This package includes classes for handling exceptions, generating IDs, and managing password encoding.
+ *
+ * @author yongjunhong
+ * @since 2024. 6. 4.
+ */
+package IDMaker.idmaker;
